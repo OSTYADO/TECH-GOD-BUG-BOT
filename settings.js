2d7842b235c41c7de4821e8243473b1a1b6baea1
OSTYADO <clamourostyado@gmail.com>
2024-09-09 00:20:21 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,8 +36,8 @@ global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autoTyping = true //auto typing
+global.autorecordtype =true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
